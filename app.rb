34a6bfb29655728637a7863a62377c6dfb6f0b64
RyanSnodgrass <res0428@yahoo.com>
2014-04-14 16:09:22 -0400

    added bootstrap

--- app.rb
+++ app.rb
@@ -5,15 +5,19 @@ get "/error" do
 end
 
 get "/fav_mov_app" do	
-	@favorites = Favorites.new
+	@favs = Favorites.favs
 	erb :"fav_mov/index"
 end
 
 class Favorites
+	attr_reader :favs
 	@@favs = ["Blade Runner", "Fight Club"]
 	def initialize
 	end
 	def self.favs
 		@@favs
+	end
+	def self.add_mov(new_mov)
+		@@favs.push(new_mov)
 	end
 end
--- spec/fav_mov_spec.rb
+++ spec/fav_mov_spec.rb
@@ -1,7 +1,15 @@
 require 'rspec'
 require './app.rb'
 
-describe FavMov do
-	it 'should have a pre-made list'
-	
+describe Favorites do
+	it 'should have a pre-made list' do
+		Favorites.favs.should_not be_empty
+	end
+	it 'should add movies' do
+		first_count = Favorites.favs.count
+		new_movie = "casablanca"
+		Favorites.add_mov(new_movie)
+		second_count = Favorites.favs.count
+		second_count.should > first_count
+	end
 end
