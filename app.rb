cdabc0c647fe8ecc2259580502545ae42046de40
RyanSnodgrass <res0428@yahoo.com>
2014-04-14 20:47:58 -0400

    additional movies now working

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+#RACK_ENV=production rackup config.ru
 get "/error" do
 	"whoopsies try again"
 end
@@ -13,9 +14,18 @@ get "/fav_mov_app" do
 	erb :"fav_mov/index"
 end
 
+post "/favs" do
+	name = params[:mov_name]
+	description = params[:mov_desc]
+	movie = {name => description}
+	
+	Favorites.add_mov(movie)
+	redirect "./fav_mov_app"
+end
+
 class Favorites
 	attr_reader :favs
-	@@favs = {"Blade Runner" => "Super awesome Cyberpunk movie directed by Ridley Scott and starring Harrison Ford", "Fight Club" => "Super awesome movie about 'the doomed generation' fighting against the established system and each other. Starring Brad Pitt and Edward Norton", "2001 Space Odyssey" => "Super trippy awesome movie directed by Stanley Kubrik showcasing a metaphorical evolution of mankind", "Spring, Summer, Fall, Winter, and Spring Again" => "Quiet meditative moving painting focusing on two monks in the ganghis mountains and the revolving nature of life."}
+	@@favs = {"Blade Runner" => "Super awesome Cyberpunk movie directed by Ridley Scott and starring Harrison Ford", "Fight Club" => "Super awesome movie about 'the doomed generation' fighting against the established system and each other. Starring Brad Pitt and Edward Norton", "2001 Space Odyssey" => "Super trippy awesome movie directed by Stanley Kubrik showcasing a metaphorical evolution of mankind", "Spring, Summer, Fall, Winter, and Spring Again" => "Quiet and meditative moving painting focusing on two monks in the ganghis mountains and the revolving nature of life."}
 	def initialize
 	end
 	def self.favs
